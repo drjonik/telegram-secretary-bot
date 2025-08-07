f6a616cee60d362f2037fafc6e5eebb1e866ab5f
drjonik <damon12sleep@gmail.com>
2025-08-07 22:45:51 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from database import init_db
 import sys
 
 def main():
-    if not BOT_TOKEN or not BOT_TOKEN.startswith(""):
+    if not BOT_TOKEN or not BOT_TOKEN.startswith("8"):
         print("❌ BOT_TOKEN не найден или некорректен. Получи токен от https://t.me/BotFather и добавь его в переменные окружения.")
         sys.exit(1)
 
@@ -22,4 +22,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
